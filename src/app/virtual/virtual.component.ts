11168ae977aa12bd819a5f159fed9cd0c3285663
Oliver <matthrews@outlook.com>
2023-04-05 18:01:21 +0800

    update

--- src/app/virtual/virtual.component.ts
+++ src/app/virtual/virtual.component.ts
@@ -1,31 +1,26 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-virtual',
   templateUrl: './virtual.component.html',
-  styleUrls: ['./virtual.component.css']
+  styleUrls: ['./virtual.component.css'],
 })
 export class VirtualComponent implements OnInit {
   @ViewChild(CdkVirtualScrollViewport)
   viewport: CdkVirtualScrollViewport;
 
-personas = Array(500).fill(0);
-  constructor() { }
+  personas = Array(500).fill(0);
+  constructor() {}
 
-  ngOnInit(): void {
-    console.log(this.personas);
-    
+  ngOnInit(): void {}
+
+  onScrolledIndexChange(): void {
+    const total = this.viewport.measureScrollOffset();
+    console.log('total', total);
+    const pos = this.viewport.getRenderedRange().start;
+    if (pos > this.personas.length / 2 && pos < this.personas.length / 2 + 10) {
+      this.personas = Array(2000).fill(0);
+    }
   }
-cambio(){
-  const total=this.viewport.measureScrollOffset();
-  console.log("total",total)
-  const posicion = this.viewport.getRenderedRange().start;
-  console.log(posicion)
-  console.log("personas " ,this.personas.length)
-  if((posicion>this.personas.length/2)&&(posicion<this.personas.length/2+10)){
-alert("has llegado casi a la mitad");
-this.personas = Array(800).fill(0);
-  }
-}
 }
